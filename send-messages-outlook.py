1a60a434828958486a62635ad0a3cac3ed6f7878
Vishnu <Sabari.Vishnu.Jayanthan.Jaikrishnan@wdc.com>
2024-06-25 22:02:22 +0800

    added outlook mailing

---
+++ send-messages-outlook.py
@@ -0,0 +1,39 @@
+# Description: Send email using O365 and redmail library
+
+# from O365 import Message, Account, MSGraphProtocol
+
+# html_template = """
+#                 <!DOCTYPE html>
+#                 <html>
+#                 <head>
+#                     <title>Notification</title>
+#                 </head>
+#                 <body>
+#                     <h1>Notification</h1>
+#                     <p>This is a notification email.</p>
+#                 </body>
+#                 </html>
+#                 """
+
+# O365_AUTH = ('', '')
+
+# protocol = MSGraphProtocol()
+# account = Account(credentials=O365_AUTH, protocol=protocol)
+
+# mbox = account.mailbox()
+# m = mbox.new_message()
+
+# # Set the recipients, subject, and body of the message
+# m.to.add('')
+# m.subject = ''
+# m.body = html_template  # Assuming html_template is a string containing the HTML body
+# m.send()
+
+
+# using 3rd party library
+from redmail import outlook
+outlook.username = ""
+outlook.password = ""
+outlook.send(receivers = [""],
+             subject = "Dummy Notification",
+             text = "This is a notification email.")
